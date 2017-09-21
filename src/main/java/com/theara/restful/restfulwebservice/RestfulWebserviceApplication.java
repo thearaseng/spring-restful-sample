348a30750743e947ebfe12df474dffcdfe689a13
Theara Seng <seng.theara.kh@gmail.com>
2017-09-21 18:28:46 +0700

    initial commit

---
+++ src/main/java/com/theara/restful/restfulwebservice/RestfulWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.theara.restful.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/theara/restful/restfulwebservice/RestfulWebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theara.restful.restfulwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
