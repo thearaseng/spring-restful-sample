83e5a9a5bd7e7ea65d4fdcdf41766a18f93596b4
Theara Seng <seng.theara.kh@gmail.com>
2017-09-23 19:52:05 +0700

    enhance Swagger document with custom annotations

--- src/main/java/com/theara/restful/restfulwebservice/model/User.java
+++ src/main/java/com/theara/restful/restfulwebservice/model/User.java
@@ -1,17 +1,23 @@
 package com.theara.restful.restfulwebservice.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user")
 public class User {
 
     private Integer id;
 
     @Size(min = 3, message = "Name should be more than 3 characters")
+    @ApiModelProperty(notes = "Name should be more than 3 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birthday should be in the past")
     private Date birthDate;
 
     public User(){
